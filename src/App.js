94c8d368e225e641c0579db421becdd7e751dce0
Nischal Bhandari <niscalbhandari12@gmail.com>
2019-08-25 21:51:04 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
+import About from './About';
+import Navigation from './Navigation';
+import Services from './Services';
+import Home from './Home';
 import './App.css';
+import ShowChart from './ShowChart';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+import {Row,Col} from 'reactstrap';
+import Contact from './contacts';
+import SignUp from './SignUp';
+import {Switch ,Route,HashRouter as Router}  from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router >
+    <Navigation />
+
+    <Route path='/' exact component={Home}/>
+   <Route path='/about' component={About}/>
+    <Route path='/services' component={Services}/>
+    <Route path='/ourWorks' component={Contact}/>
+    <Route path='/signup' component={SignUp}/>
+    
+   
+    <footer className="footer">
+    
+    <div className="footer-bar">
+    <Row>
+    <Col>
+    <div class="SocialMedia">
+
+    <h2> <a href="tel:01-549074"><i class="material-icons" >local_phone</i> Contact: 01-549074</a></h2>
+    <h2> <a href="mailto:xyz@abc.com"><i class="material-icons" >email</i>email: xyz@abc.com</a></h2>
+  </div>
+    </Col>
+    <Col>
+    <div class="SocialMedia">
+    <a href="#" class="fa fa-facebook"></a>
+    <a href="#" class="fa fa-twitter"></a>
+    <a href="#" class="fa fa-instagram"></a>
     </div>
+    </Col>
+    </Row>
+        <MessengerCustomerChat
+    pageId="173630249340239"
+    />
+    
+    </div>
+
+    </footer>
+    </Router>
+
   );
 }
 
